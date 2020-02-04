62eb42234c776ee640a5982ce4b11e51a418315e
Micah Banschick <micha.ban@gmail.com>
2020-02-04 15:57:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,52 @@
-
+class CashRegister
+  
+  attr_accessor :total 
+  attr_reader :discount 
+  
+  def initialize(discount = nil) 
+    @total = 0
+    @discount = discount 
+    @items = []
+    @list = []
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    i = 0
+    while i < quantity do 
+      @items << title
+      i += 1
+    end 
+    @list << [title, price, quantity]
+    @total += price * quantity
+  end 
+  
+  def apply_discount
+    if !!@discount 
+      @total -= @total * @discount/100
+      return "After the discount, the total comes to $800."
+    else 
+      return "There is no discount to apply."
+    end 
+  end 
+  
+  def list 
+    @list 
+  end 
+  
+  def items
+    @items
+  end 
+  
+  def void_last_transaction
+    if @items.length > 0 
+      @total -= @list[@list.length - 1][1]
+      @items.pop()
+    elsif @items[0] == @items[@items.length - 1]
+      @items.pop()
+      @total = 0.0
+    end 
+      
+  end 
+    
+  
+end
